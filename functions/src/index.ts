4ae237d7f2940f61753e1055153dbbb94948da39
Med <kherfallaouimohamed@gmail.com>
2020-11-08 15:57:58 +0100

    notification march

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+//const db=admin.firestore();
+const fcm= admin.messaging();
+
+export const sendToken=functions.firestore
+.document('Commande/{CommandeId}')
+.onUpdate(async snapshot =>{
+    const tkn = new Array();
+    const oldSnap = snapshot.before.data(); // previous document        
+    const newSnap = snapshot.after.data(); // current document       
+   if(newSnap.EtatCommande === true && oldSnap.EtatCommande===false )
+   {
+       tkn.push(newSnap.Token);
+       console.log("kikouuuuuuuuuu"+tkn[0]);
+   } 
+   const payload : admin.messaging.MessagingPayload={
+       notification : {
+           title :'Commande wejdet',
+           body:'arwaho ediw wre9ikom',
+           clickAction:'FLUTTER_NOTIFICATION_CLICK',
+       },
+   } ;
+   return fcm.sendToDevice(tkn,payload)
+
+})
+
+ //export const helloWorld = functions.https.onRequest((request, response) => {functions.logger.info("Hello logs!", {structuredData: true});
+   //response.send("Hello from Firebase!");
+ //});
